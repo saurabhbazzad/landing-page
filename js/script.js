bd8fda73f1022b5e7add17100f7f2f0ce537a305
Saurabh <saurabhbazzad@gmail.com>
2019-07-07 13:24:46 +0530

    TODO- 3rd level nesting of inside nav bar

--- js/script.js
+++ js/script.js
@@ -7,4 +7,36 @@ $(()=>{
         $(this).removeClass('btn-outline-danger')
         $(this).addClass('btn-outline-light')
     })
+
+    // $('.dropdown-submenu a.test').on("mouseenter", function(e){
+    //     $(this).next('ul').toggle();
+    //     e.stopPropagation();
+    //     e.preventDefault();
+    // });
+    // $('.dropdown-submenu a.test').on("mouseleave",function(e){
+    //     $(this).next('ul').toggle()
+    // })
+
+    $('.test2').on('mouseenter',function(e){
+        $(this).children('div').toggle()
+    })
+    $('.test2').on('mouseleave',function(e){
+        $(this).children('div').toggle()
+    })
+
+    $('.test-special').on('mouseenter',function(e){
+        $(this).children('ul').toggle()
+    })
+    $('.test-special').on('mouseleave',function(e){
+        $(this).children('ul').toggle()
+       
+    })
+
+
+    $('.test').hover(function(e){
+        $(this).next('ul').toggle()
+    },function(e){
+        $(this).next('ul').toggle()
+    })
+
 })
